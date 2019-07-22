aa7ae3799b3a814c36e9621b436ad9849d2d62a6
Gurpreet-Fix <51693294+Gurpreet-Fix@users.noreply.github.com>
2019-07-22 13:34:48 +0530

    last project
    deleted project

--- includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$dbservername= "localhost";
-	$dbusername= "root";
-	$dbpassword= "";
-	$dbname= "loginsystem";
-	
-	$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
-	
